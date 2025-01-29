762209a7ccd7dec629b2532a9edd8ea5721e85a0
Bryan-Eka-Santoso <bryanekasantoso@gmail.com>
2025-01-29 20:37:32 +0700

    add images for projects page and more

---
+++ assets/script.js
@@ -0,0 +1,198 @@
+        // For All Pages
+        const about = document.querySelectorAll("#about");
+        const resume = document.querySelectorAll("#resume");
+        const projects = document.querySelectorAll("#projects");
+        const contact = document.querySelectorAll("#contact");
+
+        function clickAbout(){
+          about.forEach(element => {
+            element.style.display = "block";
+          });
+          resume.forEach(element => {
+            element.style.display = "none";
+          });
+          projects.forEach(element => {
+            element.style.display = "none";
+          });
+          contact.forEach(element => {
+            element.style.display = "none";
+          });
+        }
+
+        function clickResume(){
+          about.forEach(element => {
+            element.style.display = "none";
+          });
+          resume.forEach(element => {
+            element.style.display = "block";
+          });
+          projects.forEach(element => {
+            element.style.display = "none";
+          });
+          contact.forEach(element => {
+            element.style.display = "none";
+          });
+        }
+
+        function clickProjects(){
+          about.forEach(element => {
+            element.style.display = "none";
+          });
+          resume.forEach(element => {
+            element.style.display = "none";
+          });
+          projects.forEach(element => {
+            element.style.display = "block";
+          });
+          contact.forEach(element => {
+            element.style.display = "none";
+          });
+        }
+
+        function clickContact(){
+          about.forEach(element => {
+            element.style.display = "none";
+          });
+          resume.forEach(element => {
+            element.style.display = "none";
+          });
+          projects.forEach(element => {
+            element.style.display = "none";
+          });
+          contact.forEach(element => {
+            element.style.display = "block";
+          });
+        }
+        // End
+        
+        // For Projects
+        const blog = document.querySelectorAll("#blog");
+        const webdes = document.querySelectorAll("#webdes");
+        const webdev = document.querySelectorAll("#webdev");
+
+        function callAll(){
+        blog.forEach(element => {
+          element.style.display = "block";
+        });
+        webdes.forEach(element => {
+          element.style.display = "block";
+        });
+        webdev.forEach(element => {
+          element.style.display = "block";
+        });
+      }
+
+        function callWebDes(){
+        blog.forEach(element => {
+          element.style.display = "none";
+        });
+        webdes.forEach(element => {
+          element.style.display = "block";
+        });
+        webdev.forEach(element => {
+          element.style.display = "none";
+        });
+      }
+
+        function callWebDev(){
+        blog.forEach(element => {
+          element.style.display = "none";
+        });
+        webdes.forEach(element => {
+          element.style.display = "none";
+        });
+        webdev.forEach(element => {
+          element.style.display = "block";
+        });
+      }
+
+        function callCpp(){
+        blog.forEach(element => {
+          element.style.display = "none";
+        });
+        webdes.forEach(element => {
+          element.style.display = "none";
+        });
+        webdev.forEach(element => {
+          element.style.display = "none";
+        });
+      }
+
+        function callBlog(){
+        blog.forEach(element => {
+          element.style.display = "block";
+        });
+        webdes.forEach(element => {
+          element.style.display = "none";
+        });
+        webdev.forEach(element => {
+          element.style.display = "none";
+        });
+      }
+    //   Projects End
+    // For Click Read More Text
+    const m_internship1 = document.querySelectorAll(".more-internship1");
+    const readmoreTxt = document.querySelectorAll(".readmore");
+
+    function clickIntership1(){
+      m_internship1.forEach(element => {
+        element.style.display = "block";
+      });
+      readmoreTxt.forEach(element => {
+        element.style.display = "none";
+      });
+    }
+    // End
+    // Certificates
+    const achievements = document.querySelectorAll("#achievements");
+    const events = document.querySelectorAll("#events");
+    const organizations = document.querySelectorAll("#organizations");
+
+    function clickAll(){
+      achievements.forEach(element => {
+        element.style.display = "block";
+      });
+      events.forEach(element => {
+        element.style.display = "block";
+      });
+      organizations.forEach(element => {
+        element.style.display = "block";
+      });
+    }
+
+    function clickAchievements(){
+      achievements.forEach(element => {
+        element.style.display = "block";
+      });
+      events.forEach(element => {
+        element.style.display = "none";
+      });
+      organizations.forEach(element => {
+        element.style.display = "none";
+      });
+    }
+
+    function clickEvents(){
+      achievements.forEach(element => {
+        element.style.display = "none";
+      });
+      events.forEach(element => {
+        element.style.display = "block";
+      });
+      organizations.forEach(element => {
+        element.style.display = "none";
+      });
+    }
+
+    function clickOrganizations(){
+      achievements.forEach(element => {
+        element.style.display = "none";
+      });
+      events.forEach(element => {
+        element.style.display = "none";
+      });
+      organizations.forEach(element => {
+        element.style.display = "block";
+      });
+    }
+    // End
